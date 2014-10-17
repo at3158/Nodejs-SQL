6af238187b068c77a2a79dc01852626d60b96586
jain <at3158@hotmail.com>
2014-10-17 19:27:16 +0800

    Nodejs-SQL
    Nodejs-SQL

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿var http = require('http');
+var sql = require('mssql');
+var port = process.env.port || 1337;
+
+
+
+var config = {
+    user: 'dsc',
+    password: 'dsc',
+    server: '192.168.3.4', // You can use 'localhost\\instance' to connect to named instance
+    database: 'Mayuki',
+};
+
+sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query("SELECT TOP 1 * FROM ACTMA", function (err, recordset) {
+        console.log(recordset);
+    });
+});
+
+/*
+ * http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+ * 
+ * */
